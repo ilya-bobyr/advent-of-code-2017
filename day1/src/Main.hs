f965027783b67debff45c87e33f3649c5966c9d3
Ilya Bobyr <ilya.bobyr@gmail.com>
2018-12-01 22:42:41 -0800

    Update per intero suggestions

--- day1/src/Main.hs
+++ day1/src/Main.hs
@@ -3,14 +3,14 @@ module Main where
 main :: IO ()
 main = do
   input <- getLine
-  let digits = map read $ map (\c -> [c]) input :: [Int]
+  let digits = map (read . (: [])) input :: [Int]
 
       pairs1 = zip digits (tail digits ++ [head digits])
 
       halfSize = length digits `quot` 2
       pairs2 = zip digits $ drop halfSize digits ++ take halfSize digits
 
-      answer p = sum $ map (\(x, _) -> x) $ filter (\(x, y) -> x == y) p
+      answer p = sum $ map fst $ filter (uncurry (==)) p
 
-  print $ answer pairs1 
+  print $ answer pairs1
   print $ answer pairs2
